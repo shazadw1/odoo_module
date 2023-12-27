d3c60190cf21ea6bd1f1a24cb8c55fe533bab2aa
shazadw1 <70218450+shazadw1@users.noreply.github.com>
2023-12-27 02:11:55 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,13 @@
 class puppet_odoo {
-  include install_odoo
+  # Include the install_odoo class
+  include puppet_odoo::install_odoo
+
+  # Include the install_nginx class
+  include puppet_odoo::install_nginx
+
+  # Include the manage_traffic class
+  include puppet_odoo::manage_traffic
+
+  # You can add more classes here as your module expands
 }
+
