d84e965521f5214a8830a219432a155e5123cd84
Waseem Shazad <shazadw1@gmail.com>
2023-12-30 20:15:30 +0000

    Updated init.pp with correct module name  install_odoo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,19 @@
-# puppet_odoo/manifests/sites.pp
-node default {
+# /etc/puppetlabs/code/environments/common/puppet_odoo/manifests/init.pp
+
+class puppet_odoo {
   # Include classes from puppet_odoo module
   include puppet_odoo::system_setup
-  include puppet_odoo::install
+  include puppet_odoo::install_odoo
   include puppet_odoo::install_nginx
   include puppet_odoo::set_backups
   include puppet_odoo::manage_traffic
-  
-  
   include puppet_odoo::github_access
-  #include puppet_odoo::secure_odoo
-
+  # include puppet_odoo::secure_odoo
 
   # Add other classes as needed for all nodes
+}
+
+# Node definition (if required)
+node 'd2.adaplo.io' {
+  include puppet_odoo
 }
